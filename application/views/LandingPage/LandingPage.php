1a074b37d01ad3e446cde82dbdc47c91fce19e36
aliffrhn <alif.farhanw@gmail.com>
2020-04-06 22:36:30 +0700

    setup LandingPage

---
+++ application/views/LandingPage/LandingPage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" href="<?php echo base_url('./assets/css/bootstrap.min.css')?>" />
+    
+    <!-- js -->
+    <script src="<?php base_url('./assets/js/jquery-3.4.1.js') ?>"></script>
+    <script src="<?php base_url('./assets/js/popper.js') ?>"></script>
+</head>
+<body>
+    
+</body>
+</html>
