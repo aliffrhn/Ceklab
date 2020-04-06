db441dd483f398d1bb2f29bed1d7369dd7dcc83a
aliffrhn <alif.farhanw@gmail.com>
2020-04-06 22:32:14 +0700

    change title in transaction page

--- application/views/TransactionPage/TransactionPage.php
+++ application/views/TransactionPage/TransactionPage.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>hello world</title>
+    <title>Transaction Page</title>
+
     <!-- Bootstrap css-->
     <link rel="stylesheet" href="<?php echo base_url('./assets/css/bootstrap.min.css')?>" />
     
