e587e6434d7a999900826ea274652ada899a7f86
aliffrhn <alif.farhanw@gmail.com>
2020-04-07 08:55:39 +0700

    add responsive to autoplay slider

--- application/views/LandingPage/LandingPage.php
+++ application/views/LandingPage/LandingPage.php
@@ -265,7 +265,32 @@
             speed: 2000,
             dots: false,
             prevArrow: false,
-            nextArrow: false
+            nextArrow: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
     </script>
 </body>
