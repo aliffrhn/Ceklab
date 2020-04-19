56bfd7f42815441c89f4310087ef78b4ba842403
aliffrhn <alif.farhanw@gmail.com>
2020-04-20 00:04:51 +0700

    init AdminDashboard view and controller

---
+++ application/controllers/AdminDashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+class AdminDashboard extends CI_Controller {
+    
+    public function index(){
+        $this->load->view('AdminDashboard/AdminDashboard');
+    }
+}
+
+?>
---
+++ application/views/AdminDashboard/AdminDashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" href="<?php echo base_url('./assets/css/bootstrap.min.css')?>" />
+    
+    <!-- js -->
+    <script src="<?php echo base_url('./assets/js/jquery-3.4.1.js') ?>"></script>
+    <script src="<?php echo base_url('./assets/js/popper.js') ?>"></script>
+    <script src="<?php echo base_url('./assets/js/bootstrap.min.js') ?>"></script>
+</head>
+<body>
+    <div class="container" style="margin-top: 50px;">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
